142eafa3b79a3fdd02470139f6bedf0cd32f0f72
Talasan Nicholson <talasan.nicholson@gmail.com>
2019-01-16 14:09:55 -0600

    quick gal commit

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -38,6 +38,13 @@ export default class extends Document<any> {
         <body>
           <Main />
           <NextScript />
+
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-10405648-10"
+          />
+
+          <script>{`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-10405648-10');`}</script>
         </body>
       </html>
     )
